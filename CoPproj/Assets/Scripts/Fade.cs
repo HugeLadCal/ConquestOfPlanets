0893be5ae256f411ae52a6e831507a1c202d34fe
Kh8407j <kh8407j@gre.ac.uk>
2023-11-10 16:13:48 +0000

    Turret script.

--- CoPproj/Assets/Scripts/Fade.cs
+++ CoPproj/Assets/Scripts/Fade.cs
@@ -1,4 +1,4 @@
-// KHOGDEN
+// KHOGDEN 001115381
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +7,17 @@ namespace UI
 {
     public class Fade : MonoBehaviour
     {
+        public static Fade instance;
+        [SerializeField] bool fadeOut;
+
+        private Animator anim;
+
+        // KH - Called before 'void Start()'.
+        void Awake()
+        {
+            instance = this;
+        }
+
         // Start is called before the first frame update
         void Start()
         {
@@ -16,7 +27,7 @@ namespace UI
         // Update is called once per frame
         void Update()
         {
-
+            anim.SetBool("FadeOut", fadeOut);
         }
     }
 }
---
+++ CoPproj/Assets/Scripts/Turret.cs
@@ -0,0 +1,71 @@
+// KHOGDEN 001115381
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace interactable
+{
+    public class Turret : MonoBehaviour
+    {
+        [SerializeField] float interactDistance = 3f;
+        [SerializeField] GameObject cannonBeam;
+        [SerializeField] GameObject explosion;
+
+        private Transform player;
+        private PlayerUI playerUI;
+        private float dist;
+        private bool shownTip;
+
+        public AudioSource source;
+        public AudioClip explosionSound;
+
+        // KH - Called before 'void Start()'.
+        void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            playerUI = FindObjectOfType<PlayerUI>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        // KH - Called on a constant timeline.
+        void FixedUpdate()
+        {
+            // KH - Calculate distance with player and turret.
+            dist = Vector3.Distance(player.position, transform.position);
+
+            // KH - Check that player is in interacting distance with turret.
+            if(dist < interactDistance)
+            {
+                // KH - Has the game told the player to press E yet?
+                if(!shownTip)
+                {
+                    shownTip = true;
+                    playerUI.DisplayTip("Press E to fire cannon.");
+                }
+
+                // KH - Check that player has pressed E.
+                if(Input.GetKey(KeyCode.E))
+                {
+                    // KH - Display the explosion.
+                    cannonBeam.SetActive(true);
+                    explosion.SetActive(true);
+
+                    source.PlayOneShot(explosionSound);
+                }
+            }
+        }
+    }
+}
